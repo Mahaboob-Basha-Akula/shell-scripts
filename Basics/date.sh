bba82a2147efe6da166a81d32a317337c3260ff7
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-20 20:36:25 +0530

    variables used

--- Basics/date.sh
+++ Basics/date.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
-DATE=$(date +%F)
+# DATE=$(date +%F)
 
-echo "time is $DATE"
+Start_time=$(date +%s)
+
+sleep 5 &
+
+echo "script running "
+
+
+End_time=$(date +%s)
+
+echo "script completed."
+
+Total_time=$($End_time-$Start_time)
+
+echo "Script taken time is $Total_time"
