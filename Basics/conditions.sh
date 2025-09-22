762193fd91c1520af96ee0d7d8a7cd180cb2d2ee
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-22 21:41:47 +0530

    conditions  used

--- Basics/conditions.sh
+++ Basics/conditions.sh
@@ -4,9 +4,9 @@
 NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
-    echo "Given number is $NUMBER Is correct "
+    echo "Given number is $NUMBER Is less than 10 "
 
 else
-    echo "Given number is $NUMBER IS wrong "  
+    echo "Given number is $NUMBER IS not less than 10 "  
 
 fi    
