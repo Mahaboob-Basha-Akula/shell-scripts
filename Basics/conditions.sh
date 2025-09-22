6c78708dc364bfa5d5fd57e712523e8e8a9b3ccb
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-22 21:39:15 +0530

    conditions  used

--- Basics/conditions.sh
+++ Basics/conditions.sh
@@ -3,8 +3,8 @@
 
 NUMBER=$1
 
-if [$NUMBER lt 1]; then
-    echo "Given number is $NUMBER IS correct "
+if [$NUMBER -lt 10]; then
+    echo "Given number is $NUMBER Is correct "
 
 else
     echo "Given number is $NUMBER IS wrong "  
