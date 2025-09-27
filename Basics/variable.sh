9f7ddb031b51a1eb7907ca8ae326f46b0accc1b3
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-27 20:50:22 +0530

    conditions  used

--- Basics/variable.sh
+++ Basics/variable.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-DATE=$(date)
+Start_Time=$(date +%s)
+End_Time=$(date +%s)
+
+echo "$Start_Time"
+
+echo "script execution started "
+
+
+sleep 10s
+
+echo "$End_Time"
+
 
 echo "Timestamp : $DATE"
