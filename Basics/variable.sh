5f3a06cc1268ab6dd04af6376a27f5d9a64fc1af
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-27 21:03:38 +0530

    conditions  used

--- Basics/variable.sh
+++ Basics/variable.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+# Start_Time=$(date +%s)
+# End_Time=$(date +%s )
+
+
 Start_Time=$(date +%s)
-End_Time=$(date +%s )
-
-
-echo "$Start_Time"
 
 echo "script execution started "
 
 
 sleep 20
 
-echo "$End_Time"
+End_Time=$(date +%s )
 
 Total_Time=$(($End_Time-$Start_Time))
 
