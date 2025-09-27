71b730400a71bda857962a02dd005bbd324dd190
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-27 22:05:21 +0530

    conditions  used

--- Basics/variable.sh
+++ Basics/variable.sh
@@ -18,12 +18,18 @@
 
 # echo "Timestamp : $Total_Time in seconds"
 
-echo "All variable used are $@"
+echo "All variable used are: $@"
 
-echo "script name is $0"
+echo "script name is: $0"
 
-echo "pwd is $PWD"
+echo "pwd is: $PWD"
 
-echo "current user $USER"
+echo "current user: $USER"
 
-echo "location $HOME"
+echo "location: $HOME"
+
+echo "PID is : $$"
+
+sleep 5
+
+echo "PID of last command in background is :$! "
