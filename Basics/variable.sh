01a212424bfa76e5a5e93cfd35cfdb4f57daa11c
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-27 22:03:12 +0530

    conditions  used

--- Basics/variable.sh
+++ Basics/variable.sh
@@ -4,18 +4,26 @@
 # End_Time=$(date +%s )
 
 
-Start_Time=$(date +%s)
+# Start_Time=$(date +%s)
 
-echo "script execution started "
-
-
-sleep 20
-
-End_Time=$(date +%s )
-
-Total_Time=$(($End_Time-$Start_Time))
+# echo "script execution started "
 
 
-echo "Timestamp : $Total_Time in seconds"
+# sleep 20
+
+# End_Time=$(date +%s )
+
+# Total_Time=$(($End_Time-$Start_Time))
+
+
+# echo "Timestamp : $Total_Time in seconds"
 
 echo "All variable used are $@"
+
+echo "script name is $0"
+
+echo "pwd is $PWD"
+
+echo "current user $USER"
+
+echo "location $HOME"
