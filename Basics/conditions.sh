80108e635fb0043a1ef429a32bb1c145399c3896
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2025-09-29 20:07:46 +0530

    condtions re written

--- Basics/conditions.sh
+++ Basics/conditions.sh
@@ -4,8 +4,10 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
     echo "The given $NUMBER is less than 10"
-elif[ $NUMBER -eq 10 ]; then
+
+elif [ $NUMBER -eq 10 ]; then
     echo "The given $NUMBER is equal to 10"
+    
 else
     echo "The given $NUMBER is greater than 10"
 fi
